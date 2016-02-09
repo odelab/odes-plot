fddcbdadfb64b04da87791ced6d8cbfa3f74ba37
Jingnan Shi <jshi@g.hmc.edu>
2016-02-09 00:47:49 -0800

    dynamic d3 plot enabled

--- Examples/Live Update Graphs/upload.js
+++ Examples/Live Update Graphs/upload.js
@@ -31,5 +31,31 @@ function handleSingleFileSelect(evt, editor) {
     }
 }
 
+// handle data
+function handleDataFileSelect(evt, editor) {
+    var file = evt.target.files[0]; // first of the FileList object
+
+    if (file) {
+      var r = new FileReader();
+      r.onload = function(e) { 
+	      var contents = e.target.result;
+	      editor.setValue(contents)
+        alert( "Got data file.\n" 
+              +"name: " + file.name + "\n"
+              +"type: " + file.type + "\n"
+              +"size: " + file.size + " bytes\n"
+              + "starts with: " + contents.substr(1, contents.indexOf("\n"))
+        );  
+      }
+
+      // asynchronous readAsText, need onload callback to see the result
+      r.readAsText(file);
+      
+    } else { 
+      alert("Failed to load file");
+      return null;
+    }
+}
+
 // trigger
 // document.getElementById('dataFile').addEventListener('change', handleSingleFileSelect, false);
