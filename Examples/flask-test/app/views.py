5bfd934e6313e263b6490ce08da701834b3c3513
Jingnan Shi <jshi@g.hmc.edu>
2016-02-20 18:52:16 -0800

    bootstrap

--- Examples/flask-test/app/views.py
+++ Examples/flask-test/app/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -25,6 +26,14 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    # because this function accepts POST, when 
+    # user hit submit, the following if statement
+    # will be run
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
     return render_template('login.html', 
                            title='Sign In',
-                           form=form)
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
---
+++ Examples/flask-test/config.py
@@ -0,0 +1,8 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
