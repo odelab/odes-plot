7d489d356a99aca3013684567711dd99f9c2cec1
Jingnan Shi <jshi@g.hmc.edu>
2016-02-26 20:28:38 -0800

    simple file upload function & removed view py

--- Examples/flask-graph-test/app/__init__.py
+++ Examples/flask-graph-test/app/__init__.py
@@ -1,6 +1,45 @@
 import os, sys
 
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, redirect, url_for, render_template, flash
+from werkzeug import secure_filename
 
-from app import views
+# get the absolute path for uploading files
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static/uploads')
+
+ALLOWED_EXTENSIONS = set(['txt', 'csv', 'tsv'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/',methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template('index.html',
+                           title='d3 Notebook by ODE, UPLOADED!')
+    return render_template('index.html',
+                           title='d3 Notebook by ODE')
+
+from flask import send_from_directory
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+@app.route('/static/ace/<filename>')
+def ace(filename):
+    """ create endpoint for ace js
+    """
+    pass
+
+
+#from app import views
